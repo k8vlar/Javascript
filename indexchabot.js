bedc1181415de00bb2fb8c68cbf789ed97ceece5
k8vlar <kevin.vellard.gmail.com>
2023-12-19 15:34:56 +0100

    add algo1

---
+++ indexchabot.js
@@ -0,0 +1,7 @@
+let answer = prompt ("saisissez votre sentiment...")
+    if (answer == 'bonjour'); {
+var answer2 = prompt('Bonjour! ca va?' );}
+    if (answer2 =='oui') {
+        alert('je suis content pour toi');} 
+     else {
+        alert ("désolé, il faut que je file");}
