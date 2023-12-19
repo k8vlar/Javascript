9b0c0ff80f7ce4aa603866f689a7c74357ccdfe1
Votre Nom <kevin.vellard@gmail.com>
2023-12-19 15:34:56 +0100

    add algo1

---
+++ index4.js
@@ -0,0 +1,9 @@
+
+let answer = prompt ("saisissez quelque chose")
+  if (answer ==="bonjour") {
+    alert("bonjour a toi")
+  } else if (answer === "tu vas bien?")
+    alert("bien, et toi?")
+    else {
+    alert ("désolé, je n'ai pas compris...");
+  }
